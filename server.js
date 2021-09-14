af28094862f45b105513781f886ab88f82047574
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2021-09-14 15:12:02 +0300

    gds

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 "use strict";
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 const cors = require('cors');
 app.use(cors());
 require('dotenv').config();
-const weatherData=require('./data/weather.json');
+const weatherData = require('./data/weather.json');
 const { json } = require('express');
-const PORT=process.env.PORT;
+const PORT = process.env.PORT;
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
 
     res.status(200).send("Hello world");
 });
-
+/*
 app.get('/data',(req,res)=>{
    
     
@@ -34,36 +34,38 @@ app.get('/data',(req,res)=>{
     }
     res.status(200).json(customRespone);
 });
-
-app.get('/weather-data',(req,res)=>{
-    let lat=Number(req.query.lat);
-    let lon=Number(req.query.lon);
-    if (lat&&lon){
-        let result=[];
-        weatherData.forEach(item=>{
-            if(item.lat===lat&&item.lon===lon){
-                result.push(item)
-            }
-        })
-        let city=result[0];
-        if (result.length>0){
-            let foreCast=city.data.map(item=>{
+*/
+app.get('/weather-data', (req, res) => {
+    let lat = Number(req.query.lat);
+    let lon = Number(req.query.lon);
+    let searchQuery = req.query.searchQuery
+    if (lat && lon || searchQuery) {
+        let result = weatherData.find(item => item.city_name === searchQuery)
+        if (result) {
+            let forcast = result.data.map(item => {
                 return {
-                    date:item.datetime,
-                    description:item.weather.description
-                }
-            })
-            res.status(200).json(foreCast);
-        }else{
-            res.status(404).send("resources not found")
-        }
 
-    }else{
-        res.status(400).send("please provide correct query params");
+                    date: item.datetime,
+                    description: item.weather.description
+                }
+
+
+            })
+            res.status(200).json(forcast);
+        
+
+
+    } else {
+        res.status(404).send("resources not found")
     }
+
+
+}else {
+    res.status(400).send("please provide correct query params");
+}
 
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Listening on port ${PORT}` )
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 });
